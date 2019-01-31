b0506e928723f2f622ca79dc30ac71db95a3906e
leannemcabey <leanne.m.cabey@gmail.com>
2019-01-31 09:48:19 -0500

    Complete first iteration of user signin

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -21,9 +21,6 @@ class Signin extends Component {
       })
   }
 
-  // The below is creating erroneous user records, and setting state in store before submit is clicked.
-  // My guess is it's something to do withe the async nature of setState
-
   handleSubmit = (event, state) => {
     event.preventDefault()
     fetch('http://localhost:3000/api/v1/users')
@@ -41,15 +38,16 @@ class Signin extends Component {
             'Accept': 'application/json'
           },
           body: JSON.stringify({
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            address: this.state.address,
+            first_name: this.state.firstName,
+            last_name: this.state.lastName,
+            street_address: this.state.address,
             city: this.state.city,
             state: this.state.state,
             zipcode: this.state.zipcode,
             email: this.state.email,
             username: this.state.username,
-            password: this.state.password
+            password: this.state.password,
+            current_journal_id: null
           })
         })
         .then(r => r.json())
@@ -61,7 +59,7 @@ class Signin extends Component {
   render() {
     return (
       <div>
-        <form onClick={(event) => this.handleSubmit(event, this.state)}>
+        <form onSubmit={(event) => this.handleSubmit(event, this.state)}>
           <h1>Start JournalPost'ing</h1>
           First Name<input onChange={this.handleChange} type='text' name='firstName' value={this.state.firstName}></input><br></br>
           Last Name<input onChange={this.handleChange} type='text' name='lastName' value={this.state.lastName}></input><br></br>
@@ -75,7 +73,7 @@ class Signin extends Component {
           <button type='submit'>Submit</button>
         </form>
 
-        <form onClick={(event) => this.handleSubmit(event, this.state)}>
+        <form onSubmit={(event) => this.handleSubmit(event, this.state)}>
           <h1>Welcome Back!</h1>
           Username<input onChange={this.handleChange} name='username' type='text' value={this.state.username}></input><br></br>
           Password<input onChange={this.handleChange} name='password' type='text' value={this.state.password}></input><br></br>

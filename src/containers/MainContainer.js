a02ef99b52fddff75f0fa55fadc4550d5ca02d86
leannemcabey <leanne.m.cabey@gmail.com>
2019-02-14 10:15:12 -0500

    Begin implementing reroute to welcome

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,17 +1,29 @@
 import React, {Component} from 'react'
 import MemoryLaneContainer from '../containers/MemoryLaneContainer'
 import JournalContainer from '../containers/JournalContainer'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
 
 class MainContainer extends Component {
 
   render() {
     return (
-      <div className="grid-container">
-        <MemoryLaneContainer />
-        <JournalContainer />
+      <div>
+        {this.props.activeUserId ?
+          <div className="grid-container">
+            <MemoryLaneContainer />
+            <JournalContainer />
+          </div>
+        : <Redirect to='/welcome' />}
       </div>
     )
   }
 }
 
-export default MainContainer
+const mapStateToProps = (state) => {
+  return {
+    activeUserId: state.activeUserId
+  }
+}
+
+export default connect(mapStateToProps)(MainContainer)
